0ca504fb9e19ff257b7d784b13f3e3d70db95198
ZephireNZ <brynley+github@zephire.nz>
2024-02-03 01:08:31 +0000

    Use const for __init__

--- homeassistant/components/opnsense/__init__.py
+++ homeassistant/components/opnsense/__init__.py
@@ -11,14 +11,9 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import load_platform
 from homeassistant.helpers.typing import ConfigType
 
+from .const import CONF_API_SECRET, CONF_TRACKER_INTERFACES, DOMAIN
+
 _LOGGER = logging.getLogger(__name__)
-
-CONF_API_SECRET = "api_secret"
-CONF_TRACKER_INTERFACE = "tracker_interfaces"
-
-DOMAIN = "opnsense"
-
-OPNSENSE_DATA = DOMAIN
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -28,7 +23,7 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_API_KEY): cv.string,
                 vol.Required(CONF_API_SECRET): cv.string,
                 vol.Optional(CONF_VERIFY_SSL, default=False): cv.boolean,
-                vol.Optional(CONF_TRACKER_INTERFACE, default=[]): vol.All(
+                vol.Optional(CONF_TRACKER_INTERFACES, default=[]): vol.All(
                     cv.ensure_list, [cv.string]
                 ),
             }
@@ -46,7 +41,7 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     api_key = conf[CONF_API_KEY]
     api_secret = conf[CONF_API_SECRET]
     verify_ssl = conf[CONF_VERIFY_SSL]
-    tracker_interfaces = conf[CONF_TRACKER_INTERFACE]
+    tracker_interfaces = conf[CONF_TRACKER_INTERFACES]
 
     interfaces_client = diagnostics.InterfaceClient(
         api_key, api_secret, url, verify_ssl, timeout=20
@@ -70,9 +65,9 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
                 )
                 return False
 
-    hass.data[OPNSENSE_DATA] = {
+    hass.data[DOMAIN] = {
         "interfaces": interfaces_client,
-        CONF_TRACKER_INTERFACE: tracker_interfaces,
+        CONF_TRACKER_INTERFACES: tracker_interfaces,
     }
 
     load_platform(hass, Platform.DEVICE_TRACKER, DOMAIN, tracker_interfaces, config)
